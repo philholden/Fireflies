c03a8ee8da9db91be48e30b764faafac04528b1f
Phil Holden <phil.holden@kraya.co.uk>
2012-04-16 14:25:05 +0100

    mend

---
+++ server/public/javascripts/world.js
@@ -0,0 +1,11 @@
+function world() {
+  var w = this;
+  w.w = 800;     
+  w.h = 450;
+  w.wl = 380;   //waterlevel
+  w.av = 0.05;  //air viscosity or friction
+  w.gx = 0.00;  //gravity (horizontal gravity )
+  w.gy = 0.5;
+  w.wsx = 0.0;  //windspeed
+  w.wsy = 0.0;  
+}
