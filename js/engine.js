096c2681e2936d45db0a54f158f0997eed72c1e6
Phil Holden <phil.holden@kraya.co.uk>
2012-04-16 12:24:19 +0100

    express

--- js/engine.js
+++ js/engine.js
@@ -7,7 +7,7 @@ function engine(ga) {
   en.startTime;
   en.head = 0; //the last clean frame
   en.end = 0; //the last frame that is required
-  en.fps = 1;
+  en.fps = 25;
   en.fpms = 1000/en.fps;
   
   //create a frame
