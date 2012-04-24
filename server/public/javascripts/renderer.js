0a85a7d864761a98ce68e14678cdd79eaeeb249b
Phil Holden <phil.holden@kraya.co.uk>
2012-04-24 11:18:39 +0100

    thick me

--- server/public/javascripts/renderer.js
+++ server/public/javascripts/renderer.js
@@ -15,17 +15,24 @@ function Renderer(wrapper,w) {
   rd.drawFlies = function(en){
     //ctx.fillRect(0,0,w.w,w.h);
     ctx.drawImage(bg,0,0);
+
     var players = en.frames[en.end].players;
+    //rd.drawHalo(en.me,players.length,en);
     players.forEach(function(player,i){
       rd.drawFly(i,players.length,en);
       rd.drawReflection(i,players.length,en);
     });
+    
   };
   
   rd.drawFly = function(i,n,en) {
     var c = en.frames[en.end].players[i].fly;
     if(c.dead) {
       return;
+    }
+    ctx.lineWidth = i == en.me ? 2 : 1;
+    if(i = en.me){
+      ctx.lineWidth = 2;
     }
     ctx.beginPath();
     var hue = 360/n * i + 0;
@@ -38,6 +45,35 @@ function Renderer(wrapper,w) {
         ctx.stroke();
       }
     }
+    ctx.lineWidth = 1;
+  }
+  
+  rd.drawHalo = function(i,n,en) {
+    var c = en.frames[en.end].players[i].fly;
+    if(c.dead) {
+      return;
+    }
+    
+    ctx.lineCap = 'round';
+    ctx.lineWidth = 10;
+    ctx.beginPath();
+    
+    var hue = 360/n * i + 0;
+    for (j = 0;j < 10; j++) {
+      ctx.strokeStyle = "rgba(255,255,255,0.06)";
+
+      if((en.end - j)>0){
+        c = en.frames[en.end - j].players[i].fly;
+        ctx.lineTo(c.x,c.y);
+        ctx.lineCap = 'round';
+        ctx.lineWidth = (10-j)/2;
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.lineTo(c.x,c.y);
+      }
+    }
+    ctx.lineCap = 'butt';
+    ctx.lineWidth = 1;
   }
   
   rd.drawReflection = function(i,n,en) {
