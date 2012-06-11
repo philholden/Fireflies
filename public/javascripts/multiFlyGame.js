2b983c71cce08db0833ef10fe5068753c165b27e
Phil Holden <phil.holden@kraya.co.uk>
2012-06-11 13:01:12 +0100

    awesome collision

--- public/javascripts/multiFlyGame.js
+++ public/javascripts/multiFlyGame.js
@@ -14,7 +14,7 @@ function Frame(p,cio,w){
     c.players.push(new Player(obj,w,cio));
   });
   c.fish = new Fish(p.fish,c.players,w);
-  collisionFlies2(p.players);
+  collisionFlies3(p.players);
   collisionFish();
   handelDeaths();
   
@@ -89,6 +89,67 @@ function Frame(p,cio,w){
         }
       }
     }
+  }
+  
+  function collisionFlies3(p2)
+  {
+    var x,y;
+    var dist =15;
+    var p = c.players;
+    var pp = p2;
+    var n = p.length;
+//    $('#col').html(n);
+    for(var j=0;j<p.length;j++)
+    {
+      if(!p[j].fly.uc && !p[j].fly.dead && !p[j].fly.invulnerable) {
+        for(var i=j+1;i<p.length;i++)
+        {
+          if (!p[i].fly.uc && !p[i].fly.dead && !p[i].fly.invulnerable)
+          { 
+            if(xCross(p[i].fly,p[j].fly,pp[i].fly,pp[j].fly,dist)
+              || hemi(p[i].fly,p[j].fly,pp[i].fly,pp[j].fly,dist)) {
+                p[j].fly.uc=250;
+            } else if (xCross(p[j].fly,p[i].fly,pp[j].fly,pp[i].fly,dist)
+              || hemi(p[j].fly,p[i].fly,pp[j].fly,pp[i].fly,dist)) {
+                p[i].fly.uc=250;
+            }
+          }
+        }
+      }
+    }
+  }
+  
+  //goes from being above to being below
+  function xCross(f1,f2,pf1,pf2,dist) {
+    //was above now below?
+    if(!(pf1.y < pf2.y && f1.y > f2.y)) {
+      return false;
+    }
+    var dy = pf1.y - f1.y;
+    var dx = pf1.x - f1.x;
+    var x = f1.x - f2.x;
+    var y = f1.y - f2.y;
+    if (dx === 0 && Math.abs(x) < dist) {
+      return true; //going stright down
+    }
+    var m = dy / dx;
+    var c =  y - m*x;
+    if (m === 0) {
+      return false;
+    }
+    var cx = -c/m;
+    return Math.abs(cx)<dist;
+  }
+  
+  //is inside the hemisphere above
+  function hemi(f1,f2,pf1,pf2,dist) {
+    //was above now above?
+    if(!(pf1.y < pf2.y && f1.y < f2.y)) {
+      return false;
+    }
+    var x = f1.x - f2.x;
+    var y = f1.y - f2.y;
+    return f1.ys > f2.ys && x*x+y*y<dist*dist;
   }
   
   function collisionFish() {
