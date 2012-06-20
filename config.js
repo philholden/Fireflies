5ce8dd928099ced61a5effe141969920cdead286
Phil Holden <phil.holden@kraya.co.uk>
2012-06-20 16:38:26 +0100

    reconfig

---
+++ config.js
@@ -0,0 +1,9 @@
+var config = {};
+config.port = 80;
+config.entrySite = 'http://fireflies.kraya.net';
+config.dbSite = 'test';
+config.dbSite = 'data.fireflies.kraya.net';
+config.dbFrag = '/workspace3/firefliescake';
+config.dbFrag = '';
+
+module.exports = config;
