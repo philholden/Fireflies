54637b518b52687fc75eb90ad7cfdb3b305a514f
Phil Holden <philip.holden@ncr.com>
2013-04-17 22:03:39 +0100

    added comments to key encoding

--- public/javascripts/keyCodec.js
+++ public/javascripts/keyCodec.js
@@ -1,34 +1,87 @@
-var KeyCodec = {}
+/**
+ * A key press is encoded as U L R D, a key release as u l r d
+ * each frame has a frame id and may have several direction events
+ * seporated by commas. e.g. 1L1000  
+ * id of the player who pressed the key 
+ * @type {Object}
+ */
+var KeyCodec = {};
 
+/**
+ * Encodes a key event to a string
+ * @param  {object} k 
+ * @return {string}
+ */
 KeyCodec.keyEncode = function(k) {
-  return en.me + k + en.frameId(); 
+  return en.me + k + en.frameId();
 };
 
+/**
+ * Decodes string of formart: 1u1000
+ * 1: player id
+ * u: direction event (up released) 
+ * 1000: frame number 
+ * 
+ * @param  {string} c encoded direction event
+ * @return {object}   direction event object
+ */
 KeyCodec.keyDecode = function(c){
   var reg = /[a-zA-Z]/;
   var k = c.split(reg);
-  
+
   return {
     k:c.match(reg)[0],
     id:k[0],
     f:k[1]
   };
-}
+};
 
+/**
+ * Takes in an array where the index represents the frame number
+ * the value is also an array of key presses objects consisting of 
+ * a direction code and an player id e.g.
+ * 
+ * io[1000] = [{k: 'u',id: 1}, {k: 'D', id: 2}]
+ *
+ * on frame 1000 player 1 released the up key 
+ * and player 2 released the down key is encoded as
+ *
+ * ...,1u1000,2D1000,...
+ * 
+ * @param  {array} io array of frame containing key presses
+ * @return {string}   encoded io events
+ */
 KeyCodec.ioEncode= function(io){
-  var i;
   var out = [];
-  var h;
-  for(i = 0;i<io.length;i++){
-    if(k = io[i]){
-      k.forEach(function(h){
-        out.push(h.id + h.k + i);
-      });
+  var i;
+  var k;
+
+  for (i = 0;i<io.length;i++){
+    k = io[i];
+    if (k){
+      for (j = 0; j < k.length; j++) {
+        out.push(k.id + K.k + i);
+      }
     }
   }
   return out.join(',');
-}
+};
 
+/**
+ * Takes in a string consisting of comma separated key events 
+ * 
+ * ...,1u1000,2D1000,...
+ * 
+ * on frame 1000 player 1 released the up key 
+ * and player 2 released the down key is decoded as
+ * 
+ * io[1000] = [{k: 'u',id: 1}, {k: 'D', id: 2}]
+ * 
+ * where the index represents the frame number
+ * 
+ * @param  {array} io array of frame containing key presses
+ * @return {string}   encoded io events
+ */
 KeyCodec.ioDecode= function(str){
   var en = str.split(',');
   var out = [];
--- public/javascripts/keyboard.js
+++ public/javascripts/keyboard.js
@@ -1,12 +1,20 @@
+ /**
+ * Translates key presses into joystick state where
+ * where up, down, left and right are true or false 
+ */
+
 function Keyboard() {
   var k = this;
+
+  //keycodes corrosponding to up, down left and right
   k.trans = {
-    "37":"l",
+    "37":"l", 
     "38":"u",
     "39":"r",
     "40":"d"
   };
   
+  //state object exposes current joystick state
   k.state = {
     "u":false,
     "d":false,
@@ -14,6 +22,12 @@ function Keyboard() {
     "r":false
   }
   
+  /**
+   * to be called on keyUp listener updates state object
+   * returns lower case letter representing direction released 
+   * @param  {number} key keycode
+   * @return {string}     u d l r or false if key not a direction
+   */
   k.keyUp = function(key) {
     key = k.trans[key];
     if(key) {
@@ -23,8 +37,14 @@ function Keyboard() {
       }
       return false;
     }
-  }
-  
+  };
+
+  /**
+   * to be called on keyDown listener updates state object
+   * returns upper case letter representing direction pressed
+   * @param  {number} key keycode
+   * @return {string}     U L R D or false if key not a direction
+   */
   k.keyDown = function(key) {
     key = k.trans[key];
     if(key) {
@@ -34,5 +54,5 @@ function Keyboard() {
       }
       return false;
     }
-  }
+  };
 }
